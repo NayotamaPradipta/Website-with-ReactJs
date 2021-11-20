343d906975a57612e88474e6673062ce661e5cde
NayotamaPradipta <n4y0t4m4@gmail.com>
2021-11-20 21:52:21 +0700

    Added 3 more movie

--- mml/src/component/content/movieData.js
+++ mml/src/component/content/movieData.js
@@ -39,6 +39,65 @@ const movieData = [
         "vote_average": 5.7,
         "vote_count": 119
       },
+      {
+        "adult": false,
+        "backdrop_path": "/efuPyboBVBKTYGslQph074LRvm0.jpg",
+        "genre_ids": [
+          878,
+          28,
+          12
+        ],
+        "id": 580489,
+        "original_language": "en",
+        "original_title": "Venom: Let There Be Carnage",
+        "overview": "Eddie Brock is still struggling to coexist with the shape-shifting extraterrestrial Venom. When deranged serial killer Cletus Kasady also becomes host to an alien symbiote, Brock and Venom must put aside their differences to stop his reign of terror.",
+        "popularity": 4497.836,
+        "poster_path": "/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
+        "release_date": "2021-09-30",
+        "title": "Venom: Let There Be Carnage",
+        "video": false,
+        "vote_average": 6.8,
+        "vote_count": 1823
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/7h5WAPAcUzOWpp2jrwHBB48790j.jpg",
+        "genre_ids": [
+          16,
+          28
+        ],
+        "id": 843241,
+        "original_language": "ja",
+        "original_title": "七つの大罪：光に呪われし者たち",
+        "overview": "Meliodas and his friends jump back into action when the new era of peace is threatened by a powerful, magical alliance that could spell the end for all.",
+        "popularity": 977.334,
+        "poster_path": "/k0ThmZQl5nHe4JefC2bXjqtgYp0.jpg",
+        "release_date": "2021-07-02",
+        "title": "The Seven Deadly Sins: Cursed by Light",
+        "video": false,
+        "vote_average": 8.2,
+        "vote_count": 217
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/cinER0ESG0eJ49kxlExM0MEWGxW.jpg",
+        "genre_ids": [
+          28,
+          12,
+          14
+        ],
+        "id": 566525,
+        "original_language": "en",
+        "original_title": "Shang-Chi and the Legend of the Ten Rings",
+        "overview": "Meliodas and his friends jump back into action when the new era of peace is threatened by a powerful, magical alliance that could spell the end for all.",
+        "popularity": 3641.782,
+        "poster_path": "/xeItgLK9qcafxbd8kYgv7XnMEog.jpg",
+        "release_date": "2021-09-01",
+        "title": "Shang-Chi and the Legend of the Ten Rings",
+        "video": false,
+        "vote_average": 7.9,
+        "vote_count": 1999
+      },
 ]
 
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w300";
